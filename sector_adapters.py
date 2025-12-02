26e80f63866764e50f58311241a1cef34c5ba807
Gary <ggao44@uwo.ca>
2025-12-02 15:53:05 -0500

    add sector_adapters.py

---
+++ sector_adapters.py
@@ -0,0 +1,14 @@
+from state import AgentState, SectorState
+
+
+def into_sector_state(parent: AgentState, child: SectorState) -> SectorState:
+    return child.model_copy(update={
+        "prompt": parent.prompt
+    })
+
+
+def out_of_sector_state(parent: AgentState, child: SectorState) -> AgentState:
+    return parent.model_copy(update={
+        "sector_result": child,
+        "route_taken": [*parent.route_taken, "sector_agent_done"],
+    })
